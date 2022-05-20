ef300b105219f2195ff7a9126c96329ee5778155
John Park <park.hokyun@gmail.com>
2022-05-20 18:05:43 -0400

    finished regex practice

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
@@ -1,15 +1,17 @@
 package ca.jrvs.apps.practice;
 
+
 public class RegexExcImp implements RegexExc {
-    public boolean matchJpeg(String filename) {
-        return filename.matches("^.*\\.(jpg|jpeg)$");
-    }
 
-    public boolean matchIp(String ip) {
-        return ip.matches("^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$");
-    }
+  public boolean matchJpeg(String filename) {
+    return filename.matches("^.*\\.(jpg|jpeg)$");
+  }
 
-    public boolean isEmptyLine(String line) {
-        return line.matches("^\\s*$");
-    }
+  public boolean matchIp(String ip) {
+    return ip.matches("^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$");
+  }
+
+  public boolean isEmptyLine(String line) {
+    return line.matches("^\\s*$");
+  }
 }
