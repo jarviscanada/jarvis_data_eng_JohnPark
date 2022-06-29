7a6f3ca070ef350b889cf460be17620ad3c187c4
John Park <park.hokyun@gmail.com>
2022-06-29 18:16:36 -0400

    working on springboot

---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/spring/TwitterCLIComponentScan.java
@@ -0,0 +1,11 @@
+package ca.jrvs.apps.twitter.spring;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TwitterCLIComponentScan {
+
+  public static void main(String[] args) {
+
+  }
+}
---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/spring/TwitterCLISpringBoot.java
@@ -0,0 +1,10 @@
+package ca.jrvs.apps.twitter.spring;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+@SpringBootApplication(scanBasePackages = "ca.jrvs.apps.twitter")
+public class TwitterCLISpringBoot {
+
+}
