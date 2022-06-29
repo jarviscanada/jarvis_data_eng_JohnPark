b19f21abc1b3399e67288077467d823ef3a26826
John Park <park.hokyun@gmail.com>
2022-06-29 09:45:07 -0400

    OddEvenTest finished

--- practice/src/test/java/ca/jrvs/practice/codingChallenge/OddEvenTest.java
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/OddEvenTest.java
@@ -5,12 +5,22 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class OddEvenTest {
+  OddEven oddEven = new OddEven();;
 
   @Test
   public void oddEvenMod() {
+    String testOdd = oddEven.oddEvenMod(1);
+    assertEquals("odd", testOdd);
+    String testEven = oddEven.oddEvenMod(2);
+    assertEquals("even", testEven);
   }
 
   @Test
   public void oddEvenBit() {
+    String testOdd = oddEven.oddEvenBit(1);
+    assertEquals("odd", testOdd);
+    String testEven = oddEven.oddEvenBit(2);
+    assertEquals("even", testEven);
+
   }
 }
