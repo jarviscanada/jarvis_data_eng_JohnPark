c2bbcb12ca171d714fa2e48c4270a46b9350c43e
John Park <park.hokyun@gmail.com>
2022-06-29 09:15:53 -0400

    add test

---
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/OddEvenTest.java
@@ -0,0 +1,16 @@
+package ca.jrvs.practice.codingChallenge;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OddEvenTest {
+
+  @Test
+  public void oddEvenMod() {
+  }
+
+  @Test
+  public void oddEvenBit() {
+  }
+}
