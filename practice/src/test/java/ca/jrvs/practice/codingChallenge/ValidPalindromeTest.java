725bb2d6a4ddd9c3aa3f2a20a7734bc0e688d5f3
John Park <park.hokyun@gmail.com>
2022-07-05 13:17:02 -0400

    fix bug

--- practice/src/test/java/ca/jrvs/practice/codingChallenge/ValidPalindromeTest.java
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/ValidPalindromeTest.java
@@ -19,10 +19,10 @@ public class ValidPalindromeTest {
 
   @Test
   public void validPalindromeRecursion() {
-    assertTrue(validPalindrome.validPalindromeTwoPointers("aba"));
-    assertTrue(validPalindrome.validPalindromeTwoPointers("dddaabaaddd"));
-    assertTrue(validPalindrome.validPalindromeTwoPointers("hello:a:olleh"));
-    assertFalse(validPalindrome.validPalindromeTwoPointers("ddabaaddd"));
-    assertFalse(validPalindrome.validPalindromeTwoPointers("ab"));
+    assertTrue(validPalindrome.validPalindromeRecursion("aba"));
+    assertTrue(validPalindrome.validPalindromeRecursion("dddaabaaddd"));
+    assertTrue(validPalindrome.validPalindromeRecursion("hello:a:olleh"));
+    assertFalse(validPalindrome.validPalindromeRecursion("ddabaaddd"));
+    assertFalse(validPalindrome.validPalindromeRecursion("ab"));
   }
 }
