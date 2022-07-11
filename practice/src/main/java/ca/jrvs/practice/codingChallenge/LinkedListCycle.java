37b9a6cfc2f245728ca5a2757e5d33bd1f47b30c
John Park <park.hokyun@gmail.com>
2022-07-11 10:53:52 -0400

    forgot comment

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/LinkedListCycle.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/LinkedListCycle.java
@@ -5,6 +5,13 @@ package ca.jrvs.practice.codingChallenge;
  */
 public class LinkedListCycle {
 
+  /**
+   * Big-O: O(n)
+   * Justification: If there is no cycle, fast will iterate at most n/2 and end. When there is cycle
+   * fast will go through a cycle within n/2 iteration and slow will cycle in n iteration. Thus,
+   * within 3 cycle, fast will catch upto slow and be compared to each other that is 3*n literation
+   * which is O(n)
+   */
   public boolean hasCycle(ListNode head) {
     ListNode slow = head;
     ListNode fast = head;
