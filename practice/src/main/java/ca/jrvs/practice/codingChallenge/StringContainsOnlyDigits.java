56c3be732fbcd4ffdc85669b194563ebecde7c4e
John Park <park.hokyun@gmail.com>
2022-07-12 15:10:22 -0400

    transfer purpose

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/StringContainsOnlyDigits.java
@@ -0,0 +1,23 @@
+package ca.jrvs.practice.codingChallenge;
+
+public class StringContainsOnlyDigits {
+
+  public boolean usingASCII(String s) {
+
+
+
+    return false;
+  }
+
+  public boolean usingJavaAPI(String s) {
+    return false;
+  }
+
+  public boolean usingRegex(String s) {
+    return false;
+  }
+
+  public static void main(String[] args) {
+    StringContainsOnlyDigits stringContainsOnlyDigits = new StringContainsOnlyDigits();
+  }
+}
