a7c2103114458881b2078231881f5ea93c4ae37d
John Park <park.hokyun@gmail.com>
2022-07-22 10:50:36 -0400

    forgot annotation

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/DuplicatesFromSortedArray.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/DuplicatesFromSortedArray.java
@@ -1,7 +1,14 @@
 package ca.jrvs.practice.codingChallenge;
 
+/**
+ * ticket: https://www.notion.so/jarvisdev/Contains-Duplicate-3295353a471047f68c4e17fedc827a5b
+ */
 public class DuplicatesFromSortedArray {
 
+  /**
+   * Big-O: O(n)
+   * Justification: Iterates the array nums once
+   */
   public int TwoPointers(int[] nums) {
     int i = 0;
     for (int j = 1; j < nums.length; j++) {
