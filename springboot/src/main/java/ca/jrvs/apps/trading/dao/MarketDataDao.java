52bb726980de7a4a7f1c3ab6b86910639395a66a
John Park <park.hokyun@gmail.com>
2022-08-08 21:42:22 -0400

    transfer

--- springboot/src/main/java/ca/jrvs/apps/trading/dao/MarketDataDao.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/dao/MarketDataDao.java
@@ -2,11 +2,16 @@ package ca.jrvs.apps.trading.dao;
 
 import ca.jrvs.apps.trading.model.config.MarketDataConfig;
 import ca.jrvs.apps.trading.model.domain.IexQuote;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.dao.DataRetrievalFailureException;
 
 public class MarketDataDao implements CrudRepository<IexQuote, String> {
 
@@ -22,58 +27,108 @@ public class MarketDataDao implements CrudRepository<IexQuote, String> {
     IEX_BATCH_URL = marketDataConfig.getHost() + IEX_BATCH_PATCH + marketDataConfig.getToken();
   }
 
+
+
+  /**
+   * Get an IexQuote (helper method which class findAllById)
+   * @param ticker
+   * @throws IllegalArgumentException if a given ticker is invalid
+   * @throws DataRetrievalFailureException if HTTP request failed
+   */
+  @Override
+  public Optional<IexQuote> findById(String ticker) {
+    Optional<IexQuote> iexQuote;
+    List<IexQuote> quotes = findAllById(Collections.singletonList(ticker));
+
+    if (quotes.size() == 0) {
+      return Optional.empty();
+    } else if (quotes.size() == 1) {
+      iexQuote = Optional.of(quotes.get(0));
+    } else {
+      throw new DataRetrievalFailureException("Unexpected number of quotes");
+    }
+    return iexQuote;
+  }
+
+  /**
+   * Get quotes from IEX
+   * @param tickers is a list of tickers
+   * @return a list of IexQuote object
+   * @throws IllegalArgumentException if any ticker is invalid or tickers is empty
+   * @throws  DataRetrievalFailureException if HTTP request failed
+   */
+  @Override
+  public List<IexQuote> findAllById(Iterable<String> tickers) {
+    return null;
+  }
+
+  /**
+   * Execute a get and return http entity/body as a string
+   *
+   * Tip: use EntityUtils.toString to process HTTP entity
+   *
+   * @param url resource URL
+   * @return http response body or Optional.empty for 404 response
+   * @throws DataRetrievalFailureException if HTTP failed or status code is unexpected
+   */
+  private Optional<String> executeHttpGet(String url) {
+    
+  }
+
+  /**
+   * Borrow a HTTP client from the httpClientConnectionManager
+   * @return a httpClient
+   */
+  private CloseableHttpClient getHttpClient() {
+    return HttpClients.custom()
+        .setConnectionManager(httpClientConnectionManager)
+        //prevent connectionManager shutdown when calling httpClient.close()
+        .setConnectionManagerShared(true)
+        .build();
+  }
+
   @Override
   public <S extends IexQuote> S save(S entity) {
-    return null;
+    throw new UnsupportedOperationException("Not implemented");
   }
 
   @Override
   public <S extends IexQuote> Iterable<S> saveAll(Iterable<S> entities) {
-    return null;
-  }
-
-  @Override
-  public Optional<IexQuote> findById(String s) {
-    return Optional.empty();
+    throw new UnsupportedOperationException("Not implemented");
   }
 
   @Override
   public boolean existsById(String s) {
-    return false;
+    throw new UnsupportedOperationException("Not implemented");
   }
 
   @Override
   public Iterable<IexQuote> findAll() {
-    return null;
-  }
-
-  @Override
-  public Iterable<IexQuote> findAllById(Iterable<String> strings) {
-    return null;
+    throw new UnsupportedOperationException("Not implemented");
   }
 
   @Override
   public long count() {
-    return 0;
+    throw new UnsupportedOperationException("Not implemented");
   }
 
   @Override
   public void deleteById(String s) {
-
+    throw new UnsupportedOperationException("Not implemented");
   }
 
   @Override
   public void delete(IexQuote entity) {
-
+    throw new UnsupportedOperationException("Not implemented");
   }
 
   @Override
   public void deleteAll(Iterable<? extends IexQuote> entities) {
-
+    throw new UnsupportedOperationException("Not implemented");
   }
 
   @Override
   public void deleteAll() {
-
+    throw new UnsupportedOperationException("Not implemented");
   }
 }
